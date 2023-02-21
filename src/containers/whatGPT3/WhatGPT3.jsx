e470f79e43f3cb54d80a03c1fb97336460fb765d
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-21 18:17:23 +0500

    wgpt3Section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Feature } from '../../components';
+import Features from '../features/Features';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Features/>
+      </div>
     </div>
   )
 }
