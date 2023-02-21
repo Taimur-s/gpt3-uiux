35f1433435cf5b86a23fd752edf2aa150807066f
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-21 18:24:30 +0500

    wgptCommit

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -9,6 +9,15 @@ const WhatGPT3 = () => {
       <div className='gpt3__whatgpt3-feature'>
         <Features/>
       </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
     </div>
   )
 }
