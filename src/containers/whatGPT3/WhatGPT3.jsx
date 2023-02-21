ef8085b72ee71b13ba70c26f0ec0744ca186c48a
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-21 18:40:34 +0500

    wgpt3css

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -10,7 +10,7 @@ const WhatGPT3 = () => {
         <Features/>
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
