f5effa0b0b033c076544e24af53805198bf573f8
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-21 19:59:44 +0500

    whatgpt3Final

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -8,7 +8,7 @@ const Feature = ({title, text}) => {
         <h1>{title}</h1>
       </div>
       <div className='gpt3__features-container__feature-text'>
-        {text}
+        <p>{text}</p>
       </div>
     </div>
   )
